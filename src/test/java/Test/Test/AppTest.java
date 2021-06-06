7d189bf231566965312baefd0a4d812727828cf0
Dell <Dell@DESKTOP-I75CK6E>
2021-06-06 14:43:55 +0530

    test

---
+++ src/test/java/Test/Test/AppTest.java
@@ -0,0 +1,21 @@
+package Test.Test;
+
+
+import org.testng.annotations.Test;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+       System.out.println("Hey");
+    }
+}
