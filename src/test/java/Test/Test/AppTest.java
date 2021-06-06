93c9ce98f664ba163bbe28c27f4c9116ef07cf8a
Dell <Dell@DESKTOP-I75CK6E>
2021-06-06 19:05:18 +0530

    Test

--- src/test/java/Test/Test/AppTest.java
+++ src/test/java/Test/Test/AppTest.java
@@ -1,6 +1,8 @@
 package Test.Test;
 
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 
@@ -16,6 +18,8 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-       System.out.println("Hey");
+     	System.setProperty("webdriver.chrome.driver", "E:\\Software\\chromedriver.exe");
+    		WebDriver driver=new ChromeDriver();
+    		driver.get("https://www.makemytrip.com/");
     }
 }
